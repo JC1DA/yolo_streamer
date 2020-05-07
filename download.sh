ab9d50ce6cb69c6c14ae2303694901f82c4acd5f
JC1DA <jc1da.3011@gmail.com>
2020-05-07 15:37:49 -0700

    create directories before downloading

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p models
 wget http://meowtek.art/jc1da/yolov4_320_norm.pb --no-check-certificate -O models/yolov4_320_norm.pb
 
+mkdir -p music
 wget https://meowtek.art/jc1da/music.mp3 --no-check-certificate -O music/music.mp3
